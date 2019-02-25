06c887ad6b4c8c59bddaa52f273bd9d815b21acb
Nanderson Castro <nandokstro@gmail.com>
2019-02-25 12:37:19 -0300

    Adicionando codigos do modulo do QueyBuilder

---
+++ Components/QueryBuilder/src/QueryBuilder/Select.php
@@ -0,0 +1,63 @@
+<?php
+namespace Code\QueryBuilder;
+
+class Select
+{
+	private $query;
+
+	private $where;
+
+	private $orderBy;
+
+	private $limit;
+
+	private $join;
+
+	public function __construct($table)
+	{
+		$this->query = 'SELECT * FROM ' . $table;
+	}
+
+	public function where($field, $operator, $bind = null, $concat = 'AND')
+	{
+		$bind = is_null($bind) ? ':' . $field : $bind;
+
+		if(!$this->where) {
+			$this->where .= ' WHERE ' . $field . ' ' . $operator . ' ' . $bind;
+		} else {
+			$this->where .= ' ' . $concat . ' ' . $field . ' ' . $operator . ' ' . $bind;
+		}
+
+		return $this;
+	}
+
+	public function orderBy($field, $order)
+	{
+		$this->orderBy = ' ORDER BY ' . $field . ' ' . $order;
+
+		return $this;
+	}
+
+	public function limit($kip, $take)
+	{
+		$this->limit = ' LIMIT ' . $kip . ', ' . $take;
+
+		return $this;
+	}
+
+	public function join($joinType, $table, $foreignKey, $operator, $referenceColumn, $concat = 'AND')
+	{
+		if(!$this->join) {
+			$this->join .= ' ' . $joinType . ' ' . $table . ' ON ' . $foreignKey . ' ' . $operator . ' ' . $referenceColumn;
+		} else {
+			$this->join .= ' ' . $concat . ' ' . $foreignKey . ' ' . $operator . ' ' . $referenceColumn;
+		}
+
+		return $this;
+	}
+
+	public function getSql()
+	{
+		return $this->query . $this->join . $this->where . $this->orderBy . $this->limit;
+	}
+}
---
+++ Components/QueryBuilder/tests/QueryBuilder/SelectTest.php
@@ -0,0 +1,70 @@
+<?php
+namespace CodeTests\QueryBuilder;
+
+use Code\QueryBuilder\Select;
+
+class SelectTest extends \PHPUnit\Framework\TestCase
+{
+	protected $select;
+
+	protected function assertPreConditions(): void
+	{
+		$this->assertTrue(class_exists(Select::class));
+	}
+
+	protected function setUp(): void
+	{
+		$this->select = new Select('products');
+	}
+
+	public function testIfQueryBaseIsGeneratedWithSuccess()
+	{
+		$query = $this->select->getSql();
+
+		$this->assertEquals('SELECT * FROM products', $query);
+	}
+
+	public function testIfQueryIsGeneratedWithWhereConditions()
+	{
+		$query = $this->select->where('name', '=', ':name');
+
+		$this->assertEquals('SELECT * FROM products WHERE name = :name',
+							$query->getSql());
+	}
+
+	public function testIfQueryAllowUsAddMoreConditionsInOurQueryWithWhere()
+	{
+		$query = $this->select->where('name', '=', ':name')
+							  ->where('price', '>=', ':price');
+
+		$this->assertEquals('SELECT * FROM products WHERE name = :name AND price >= :price',
+			$query->getSql());
+	}
+
+	public function testIfQueryIsGeneratedWithOrderBy()
+	{
+		$query = $this->select->orderBy('name', 'DESC');
+
+		$this->assertEquals(
+			'SELECT * FROM products ORDER BY name DESC',
+			$query->getSql()
+			);
+	}
+
+	public function testIfQueryIsGeneratedWithLimit()
+	{
+		$query = $this->select->limit(0, 15);
+
+		$this->assertEquals('SELECT * FROM products LIMIT 0, 15', $query->getSql());
+	}
+
+	public function testIfQueryIsGeneratedWithJoinsConditions()
+	{
+		$query = $this->select->join('INNER JOIN', 'colors', 'colors.product_id', '=', 'products.id');
+
+		$this->assertEquals(
+			'SELECT * FROM products INNER JOIN colors ON colors.product_id = products.id',
+			$query->getSql()
+			);
+	}
+}
